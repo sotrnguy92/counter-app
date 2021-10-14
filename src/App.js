ea91bbfa337e8821743700e28975520ccc090a56
sotrnguy92 <sonnguyen092@gmail.com>
2021-10-13 20:58:22 -0700

    adding work with states and comments regarding hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
+
+// usestate is a React hook
+// a hook is just a function that returns something
 
 function App() {
-  return (
+
+    const [counter, setCounter] = useState(0);
+    const [hungry, setHungry] = useState(false);
+
+    //Once you return html in react you cant use JS but you can still render js in the html with {}
+    return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          {hungry ? <p>I am hungry</p>: null}
+          <h1> Counter {counter}</h1>
+          <button
+              onClick={() => {
+                  setCounter(counter+1)
+              }}
+          >
+              Increment
+          </button>
+
+          <button
+              onClick={() => setCounter(counter-1)}
+          >
+              Decrement
+          </button>
+
+          <button
+            onClick={() => setHungry(!hungry)}
+          >
+              make hungry
+          </button>
+
       </header>
     </div>
   );
